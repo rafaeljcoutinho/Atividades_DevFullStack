b46367cb20da2f5ced38d02abff60e521be8abc5
Rafael Coutinho <rafaeljcoutinhoo@gmail.com>
2023-05-24 16:03:24 -0300

    update

--- atividade01/src/main/java/com/example/application/views/main/MainView.java
+++ atividade01/src/main/java/com/example/application/views/main/MainView.java
@@ -2,33 +2,31 @@ package com.example.application.views.main;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@Route("") 
-public class MainView extends VerticalLayout { 
+@PageTitle("Main")
+@Route(value = "")
+public class MainView extends HorizontalLayout {
 
-  public MainView() {
-    VerticalLayout todosList = new VerticalLayout(); 
-    TextField taskField = new TextField(); 
-    Button addButton = new Button("Add"); 
-    addButton.addClickListener(click -> { 
-      Checkbox checkbox = new Checkbox(taskField.getValue());
-      todosList.add(checkbox);
-    });
-    addButton.addClickShortcut(Key.ENTER); 
+    private TextField name;
+    private Button sayHello;
 
-    add( 
-      new H1("Vaadin Todo"),
-      todosList,
-      new HorizontalLayout(
-        taskField,
-        addButton
-      )
-    );
-  }
+    public MainView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
 }
